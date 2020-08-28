999e49aa4a9a6b479bc665053cdb6161f796de79
Jared C. Gober <jcgober@Jareds-MacBook-Air.local>
2020-08-27 20:06:49 -0400

    Added hero method 'examine'

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
-// Constructor for heros
+// Constructor for heroes
 class Hero{
-    constructor(name, description, weapon, hitPoints, acuity, speed, wisdom, defence, magic, strength){
+    constructor(name, description, gold, weapon, hitPoints, acuity, speed, wisdom, defence, magic, strength){
         this.name = name;
         this.description = description;
+        this.gold = gold
         this. weapon = weapon;
         this.hitPoints = hitPoints;
         this.acuity = acuity;
@@ -14,21 +15,29 @@ class Hero{
         this.strength = strength;
     }
     
-    // methods for heros
+    // methods for heroes
     greet(target) {
 
-        console.log(`${this.name} says hello to ${target.name}`)
+        console.log(`${this.name} says Greetings to ${target.name}`)
+    }
+
+    examine(target){
+
+        console.log(`${this.name} examines ${target.name}. Description: ${target.description}. Hit points: ${target.hitPoints}.`)
 
     }
 }
 
 
 //Character creation 
-var cor = new Hero("Corinthe", "Tribe leader", "None", 20, 10, 8, 10, 10, 10, 7)
+var cor = new Hero("Corinthe", "Tribe leader", 100, "None", 20, 10, 8, 10, 10, 10, 7)
 
-var kel = new Hero("Kellian", "son of Corinthe", "None", 15, 6, 8, 5, 6, 9, 6)
+var kel = new Hero("Kellian", "son of Corinthe", 100, "None", 15, 6, 8, 5, 6, 9, 6)
+
 
 
 //test zone
 
 cor.greet(kel)
+
+kel.examine(cor)
